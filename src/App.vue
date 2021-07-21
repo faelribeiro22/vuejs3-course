4fd95c7096d978566b826d0abef5a73ba76d85c6
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2021-07-20 22:03:00 -0300

    feat: get token func

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,32 @@
 </template>
 
 <script>
+import { watch } from "@vue/runtime-core";
+import { useRoute, useRouter } from "vue-router";
 import ModalFactory from "./components/ModalFactory";
+import services from "./services";
 export default {
   components: { ModalFactory },
+  setup() {
+    const router = useRouter();
+    const route = useRoute();
+
+    watch(
+      () => route.path,
+      async () => {
+        if (route.meta.hasAuth) {
+          const token = window.localStorage.getItem("token");
+
+          if (!token) {
+            router.push({ name: "Home" });
+            return;
+          }
+
+          const { data } = await services.users.getMe();
+          console.log("data", data);
+        }
+      }
+    );
+  },
 };
 </script>
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
+import router from "@/router";
 import axios from "axios";
 import AuthService from "./auth";
+import UsersServices from "./users";
 
 const API_ENVS = {
   local: "http://localhost:3000",
@@ -7,6 +9,14 @@ const API_ENVS = {
 
 const httpClient = axios.create({
   baseURL: API_ENVS.local,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem("token");
+  if (token) {
+    config.headers.common.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 httpClient.interceptors.response.use(
@@ -17,10 +27,15 @@ httpClient.interceptors.response.use(
     if (canThrowAnError) {
       throw new Error(error.message);
     }
+
+    if (error.response.status === 401) {
+      router.push({ name: "Home" });
+    }
     return error;
   }
 );
 
 export default {
   auth: new AuthService(httpClient),
+  users: new UsersServices(httpClient),
 };
